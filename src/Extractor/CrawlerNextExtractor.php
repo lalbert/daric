c1c043cda1ea7fb86cee3c532aed837b8fce20ed
lalbert <lucalbert66@gmail.com>
2016-05-24 14:55:52 +0200

    Add new Extractors
    * CrawlerNextExtractor and CrawlerPreviousExtractor : allow to get next
    et previous sibling node
    * RegexExtractor : allow to extract data with a regex

---
+++ src/Extractor/CrawlerNextExtractor.php
@@ -0,0 +1,42 @@
+<?php
+namespace Daric\Extractor;
+
+use Symfony\Component\DomCrawler\Crawler;
+
+class CrawlerNextExtractor implements ExtractorInterface
+{
+    /**
+     * @var string
+     */
+    protected $matchStrategy = 'first';
+
+    /**
+     * @var int
+     */
+    protected $matchIndex = 0;
+
+    /**
+     * @param string $matchStrategy
+     * @param number $matchIndex
+     */
+    public function __construct($matchStrategy = 'first', $matchIndex = 0)
+    {
+        $this->matchStrategy = $matchStrategy;
+        $this->matchIndex = $matchIndex;
+    }
+
+    public function extract($content)
+    {
+
+        if (!($content instanceof Crawler)) {
+            throw new \InvalidArgumentException('$content must be an instance of Symfony\Component\DomCrawler\Crawler');
+        }
+
+        if (!count($content)) {
+            return;
+        }
+
+        $next = $content->nextAll();
+        return $next;
+    }
+}
---
+++ src/Extractor/RegexExtractor.php
@@ -0,0 +1,35 @@
+<?php
+namespace Daric\Extractor;
+
+class RegexExtractor implements ExtractorInterface
+{
+
+    protected $pattern;
+
+    public function __construct($pattern) {
+        $this->pattern = $pattern;
+    }
+
+    /**
+     *
+     * {@inheritDoc}
+     *
+     * @see \Daric\Extractor\ExtractorInterface::extract()
+     */
+    public function extract($content)
+    {
+        $value = null;
+        if(\is_array($content)) {
+            foreach ($content as $v) {
+                $value[] = $this->extract($v);
+            }
+        } else {
+            if(false !== \preg_match($this->pattern, $content, $matches)) {
+                $value = $matches[1];
+            }
+        }
+
+        return $value;
+    }
+
+}
